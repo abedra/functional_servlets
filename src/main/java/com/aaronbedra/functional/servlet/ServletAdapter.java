7019f0ec54aa083edd7865ade10b474c93818df4
Aaron Bedra <aaron@aaronbedra.com>
2020-10-31 13:38:40 -0500

    ServletAdapter must be fixed to IO

--- src/main/java/com/aaronbedra/functional/servlet/ServletAdapter.java
+++ src/main/java/com/aaronbedra/functional/servlet/ServletAdapter.java
@@ -2,61 +2,83 @@ package com.aaronbedra.functional.servlet;
 
 import com.jnape.palatable.lambda.adt.Either;
 import com.jnape.palatable.lambda.adt.Unit;
-import com.jnape.palatable.lambda.monad.MonadRec;
+import com.jnape.palatable.lambda.io.IO;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public final class ServletAdapter<M extends MonadRec<?, M>, F, A> extends HttpServlet {
-    private final ServletMethods<M, F, A> delegate;
+public final class ServletAdapter<F, A> extends HttpServlet {
+    private final ServletMethods<IO<?>, F, A> delegate;
 
-    private ServletAdapter(ServletMethods<M, F, A> delegate) {
+    private ServletAdapter(ServletMethods<IO<?>, F, A> delegate) {
         this.delegate = delegate;
     }
 
-    public static <M extends MonadRec<?, M>, F, A> ServletAdapter<M, F, A> servletAdapter(ServletMethods<M, F, A> delegate) {
+    public static <F, A> ServletAdapter<F, A> servletAdapter(ServletMethods<IO<?>, F, A> delegate) {
         return new ServletAdapter<>(delegate);
     }
 
     @Override
     protected void doHead(HttpServletRequest req, HttpServletResponse resp) {
-        delegate.head(req, resp).flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess));
+        delegate.head(req, resp)
+                .flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess))
+                .<IO<Unit>>coerce()
+                .unsafePerformIO();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        delegate.get(req, resp).flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess));
+        delegate.get(req, resp)
+                .flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess))
+                .<IO<Unit>>coerce()
+                .unsafePerformIO();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-        delegate.post(req, resp).flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess));
+        delegate.post(req, resp)
+                .flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess))
+                .<IO<Unit>>coerce()
+                .unsafePerformIO();
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
-        delegate.put(req, resp).flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess));
+        delegate.put(req, resp)
+                .flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess))
+                .<IO<Unit>>coerce()
+                .unsafePerformIO();
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
-        delegate.delete(req, resp).flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess));
+        delegate.delete(req, resp)
+                .flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess))
+                .<IO<Unit>>coerce()
+                .unsafePerformIO();
     }
 
     @Override
     protected void doOptions(HttpServletRequest req, HttpServletResponse resp) {
-        delegate.options(req, resp).flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess));
+        delegate.options(req, resp)
+                .flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess))
+                .<IO<Unit>>coerce()
+                .unsafePerformIO();
     }
 
     @Override
     protected void doTrace(HttpServletRequest req, HttpServletResponse resp) {
-        delegate.trace(req, resp).flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess));
+        delegate.trace(req, resp)
+                .flatMap(eitherFailureSuccess -> respond(resp, eitherFailureSuccess))
+                .<IO<Unit>>coerce()
+                .unsafePerformIO();
     }
 
-    private MonadRec<Unit, M> respond(HttpServletResponse response, Either<F, A> result) {
+    private IO<Unit> respond(HttpServletResponse response, Either<F, A> result) {
         return result
                 .fmap(success -> delegate.succeed(response, success))
-                .recover(failure -> delegate.fail(response, failure));
+                .recover(failure -> delegate.fail(response, failure))
+                .coerce();
     }
 }
